40d37edebea2f611db66b4b3cb5bd07a47f11944
cromeo98 <romeo.christian98@gmail.com>
2021-06-01 16:22:14 +0200

    add: cicle for

--- gatsby-invitation/js/script.js
+++ gatsby-invitation/js/script.js
@@ -1,1 +1,20 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+var invitation = ['Anna', 'Carlo', 'Davide','Emma', 'Federico','Gaia'];
+
+var userName = prompt('La prego, inserisca il suo meraviglioso nome');
+
+var invited = false;
+
+// cicle FOR
+for (var i = 0; i < invitation.length; i++){
+    if (userName == invitation[i]){
+        invited = true
+    }
+}
+
+if (invited == true){
+    document.getElementById('result').innerHTML = 'Mi* car* ' + userName + '! Potrai partecipare agli sfarzosi festaggiamenti indetti dal Grande Gatsby.';
+} else {
+    document.getElementById('result').innerHTML = 'Il Grande Gatsby è amareggiato, ma il tuo invito dev\'essere andato perso nelle viette della Grande Mela, si occuperà di persona del suo invito ai prossimi festeggiamenti';
+}
