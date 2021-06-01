d3a366d4162d8bd86578f4ff499762e05d8fdfbb
cromeo98 <romeo.christian98@gmail.com>
2021-06-01 15:42:58 +0200

    add: prompt cicle for

---
+++ 5-num-prompt-sum-for-while/js/script.js
@@ -0,0 +1,5 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
+
+for( var i = 0 ; i < 5 ; i++){
+    var userNumber = parseInt(prompt('Inserisci un numero ' + (i+1) +'/5'))
+}
