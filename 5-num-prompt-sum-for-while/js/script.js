1da1f72658d29b6df31cf1230e710486f656a912
cromeo98 <romeo.christian98@gmail.com>
2021-06-01 15:48:29 +0200

    end snack2 (while cicle)

--- 5-num-prompt-sum-for-while/js/script.js
+++ 5-num-prompt-sum-for-while/js/script.js
@@ -1,9 +1,20 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
 var sum = 0;
 
-for( var i = 0 ; i < 5 ; i++){
+
+// cicle FOR
+// for( var i = 0 ; i < 5 ; i++){
+//     var userNumber = parseInt(prompt('Inserisci un numero ' + (i+1) +'/5'));
+//     sum += userNumber;
+// }
+
+// cicle WHILE
+var i = 0
+
+while(i < 5){
     var userNumber = parseInt(prompt('Inserisci un numero ' + (i+1) +'/5'));
     sum += userNumber;
+    i++
 }
 
 document.getElementById('sum').innerHTML = 'La somme dei numeri scelti è: ' + sum;
